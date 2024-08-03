35a493819e9b565eb854bda3d12ee50d81fc10c9
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-08-03 19:06:49 -0400

    Add resetButton to clear canvas

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const GRID_SIZE = 25;
+const DEFAULT_GRID_SIZE = 30;
 const SHADE_FACTOR = 4;
 const BASE_OPACITY = 0.1;
+let selectedGridSize;
 
-const canvasContainer = document.querySelector(".canvas-container");
-
-for (let i = 0; i < GRID_SIZE ** 2; i++) {
-  const gridItem = document.createElement("div");
-  const dimension = gridPercentage(GRID_SIZE) + "%";
-  gridItem.style.height = dimension;
-  gridItem.style.width = dimension;
-  gridItem.setAttribute("data-idx", `pixel-${i}`);
-  gridItem.setAttribute("class", "pixel");
-  canvasContainer.appendChild(gridItem);
-}
-
-function gridPercentage(rows) {
-  return 100 / rows;
-}
-
+// On mouseenter, set pixel class to black fill and increase opacity towards 1
 const fillPixel = (event) => {
   if (event.target.classList.contains("filled")) {
     const opacityModifier = (1 - BASE_OPACITY) / SHADE_FACTOR;
@@ -31,6 +17,37 @@ const fillPixel = (event) => {
   }
 };
 
-Array.from(canvasContainer.children).forEach((child) => {
-  child.addEventListener("mouseenter", fillPixel);
-});
+const clearPixel = (pixel) => {
+  console.log(pixel.classList);
+  console.log(pixel);
+  pixel.classList.remove("filled");
+};
+
+// Construct and evenly distribute pixels on canvas
+const createCanvas = (size) => {
+  const container = document.querySelector(".canvas-container");
+  let gridPercentage = (rows) => 100 / rows;
+  for (let i = 0; i < size ** 2; i++) {
+    const gridItem = document.createElement("div");
+    const dimension = gridPercentage(size) + "%";
+    gridItem.style.height = dimension;
+    gridItem.style.width = dimension;
+    gridItem.setAttribute("data-idx", `pixel-${i}`);
+    gridItem.setAttribute("class", "pixel");
+    gridItem.addEventListener("mouseenter", fillPixel);
+    container.appendChild(gridItem);
+  }
+  return container;
+};
+
+const canvasContainer = createCanvas(selectedGridSize || DEFAULT_GRID_SIZE);
+
+// Functionality
+
+const clearGrid = () => {
+  console.log("cleargrid");
+  Array.from(canvasContainer.children).forEach(clearPixel);
+};
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", clearGrid);
