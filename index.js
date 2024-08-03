4cb9462be3b8983a60c00171308352513c25f101
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-08-03 16:20:14 -0400

    Implement simple black shading behavior on pixel mouseenter

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-// Create a grid using GRID_SIZE
-const GRID_SIZE = 16;
+const GRID_SIZE = 25;
+const SHADE_FACTOR = 4;
+const BASE_OPACITY = 0.1;
 
 const canvasContainer = document.querySelector(".canvas-container");
 
-for (let i = 0; i < GRID_SIZE; i++) {
+for (let i = 0; i < GRID_SIZE ** 2; i++) {
   const gridItem = document.createElement("div");
-  const dimension = gridPercentage(Math.sqrt(GRID_SIZE)) + "%";
+  const dimension = gridPercentage(GRID_SIZE) + "%";
   gridItem.style.height = dimension;
   gridItem.style.width = dimension;
   gridItem.setAttribute("data-idx", `pixel-${i}`);
   gridItem.setAttribute("class", "pixel");
-  gridItem.textContent = gridItem.getAttribute("data-idx");
-  console.log(gridItem);
-  gridItem.style.border = "1px solid green";
   canvasContainer.appendChild(gridItem);
 }
 
 function gridPercentage(rows) {
   return 100 / rows;
 }
+
+const fillPixel = (event) => {
+  if (event.target.classList.contains("filled")) {
+    const opacityModifier = (1 - BASE_OPACITY) / SHADE_FACTOR;
+    event.target.style.opacity = Math.min(
+      +event.target.style.opacity + opacityModifier,
+      1
+    );
+  } else {
+    event.target.classList.add("filled");
+    event.target.style.opacity = BASE_OPACITY;
+  }
+};
+
+Array.from(canvasContainer.children).forEach((child) => {
+  child.addEventListener("mouseenter", fillPixel);
+});
