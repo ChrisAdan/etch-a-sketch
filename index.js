98ac34e6091976960ae58836500d6bb0e8910b79
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-08-04 15:39:40 -0400

    [Feat] Add randomize button

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const SHADE_FACTOR = 4;
 const BASE_OPACITY = 0.1;
 const DEFAULT_PIXEL_FILL = "#000000";
 const DEFAULT_PIXEL_EMPTY = "#FFFFFF";
-let selectedGridSize;
+const RGB_MAX = 255;
 let selectedTrailColor;
 let activeDrag = true;
 let canvasContainer;
 let currentCanvasSize = DEFAULT_GRID_SIZE;
+let selectedGridSize = currentCanvasSize;
 
 function initialize() {
   const gridSizeInput = document.querySelector(".select-size");
@@ -43,7 +44,7 @@ const clearPixel = (pixel) => {
 };
 
 // Construct and evenly distribute pixels on canvas
-const createCanvas = (size) => {
+const createCanvas = (size, randomize = false) => {
   const container = document.querySelector(".canvas-container");
   let gridPercentage = (rows) => 100 / rows;
   const newCanvasSize = size ** 2;
@@ -55,7 +56,16 @@ const createCanvas = (size) => {
     const dimension = gridPercentage(size) + "%";
     gridItem.style.height = dimension;
     gridItem.style.width = dimension;
-    gridItem.style.background = DEFAULT_PIXEL_EMPTY;
+    if (randomize) {
+      const [red, green, blue] = Array.from({ length: 3 }, () =>
+        Math.floor(Math.random() * RGB_MAX)
+      );
+      gridItem.style.background = `rgb(${red}, ${green}, ${blue})`;
+      gridItem.style.opacity = Math.random();
+    } else {
+      gridItem.style.background = DEFAULT_PIXEL_EMPTY;
+      gridItem.style.opacity = BASE_OPACITY;
+    }
     gridItem.setAttribute("data-idx", `pixel-${i}`);
     gridItem.setAttribute("class", "pixel");
     gridItem.addEventListener("mouseenter", fillPixel);
@@ -82,6 +92,10 @@ const generateNewGrid = (event) => {
   canvasContainer = createCanvas(selectedGridSize);
 };
 
+const randomizeGrid = () => {
+  canvasContainer = createCanvas(selectedGridSize, true);
+};
+
 // Controls
 const resetButton = document.querySelector(".reset-button");
 resetButton.addEventListener("click", clearGrid);
@@ -91,6 +105,9 @@ selectColor.addEventListener("input", setTrailColor);
 
 const gridSizeInput = document.querySelector(".select-size");
 gridSizeInput.addEventListener("input", generateNewGrid);
+
+const randomizeButton = document.querySelector(".randomize-button");
+randomizeButton.addEventListener("click", randomizeGrid);
 
 // Maybe will add click to drag back in
 // window.addEventListener("mousedown", () => (activeDrag = true));
